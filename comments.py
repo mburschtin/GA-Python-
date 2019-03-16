90da04f78e35f5f3fdb37a6a7d85c80ee9375e9a
Michelle Burschtin <michelle.burschtin@ibm.com>
2019-03-16 14:20:26 -0400

    Instructions for walking home

---
+++ comments.py
@@ -0,0 +1,36 @@
+# test
+print('hello world') # this is a comment
+
+
+
+
+# Algorithms in real-life - Objective: how to get from home from work?
+
+print('Start Walk home from work')
+print('\tInput home: home address')
+print('\tInput work: work address')
+print('\tIdentify distance north/south')
+print('\tIdentify distance east/west')
+print('\tIdentify direction north or south')
+print('\tIdentify direction east or west')
+print('If walk,')
+print('\tWalk direction north/south*distance north/south')
+print('\tWalk direction east/west*distance east/west')
+print('If subway, ')
+print('\tCheck repairs delays accidents')
+print('\tCheck subway map')
+print('\tSelect subway line')
+print('\tSelect subway direction')
+print('\tIdentify subway station')
+print('\tWalk to subway station')
+print('\tEnter subway station')
+print('\tCheck metrocard balance')
+print('\tIf metrocard balance >=2.75')
+print('\t\tEnter subway')
+print('\tIf metrocard balance <2.75')
+print('\t\tCompute missing balance=metrocard balance - 2.75')
+print('\t\tCheck bank balance for available missing balance')
+print('\t\t\tIf bank balance>=missing balance')
+print('\t\t\t\tAdd missing balance to metrocard balance')
+print('\t\t\tIf bank balance<missing balance')
+print('\t\t\t\tWalk')
